2183315871fa27ab36b29a34061322756be884fe
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-30 13:47:46 +0200

    Add files via upload

---
+++ arthimetics.py
@@ -0,0 +1,38 @@
+# two multiplication symbols makes a power relationship 
+
+powering1 = 8**2 #does 8*8
+print(powering1)
+powering2 = 8**3 # does 8*8*8
+print(powering2)
+
+
+multiplebyes = "bye " * 10 #prints "bye " repeatedly for 10 times 
+print(multiplebyes)
+
+
+#joining lists is possible
+rando = [3,4,5]
+rando2 = [1,2,4]
+combined = rando + rando2
+print(combined)
+
+#repeating sequence in lists is possible
+
+print([3,6,9] * 4)
+
+
+#you can concatenate strings using "+"
+
+hitherestranger = "hi" + " " + "there" + " " + "stranger"
+hitherestranger2 = "hi " + "there " + "stranger"
+print(hitherestranger)
+print(hitherestranger2)
+
+#remainder of 2 numbers is found using the percentage symbol
+#remainder means the number left at the end of the division 
+remainder = 5 % 2 
+print(remainder) #will print 1 
+
+#simple operations are also possible
+arithmetic = 1+2+3+4/5
+print(arithmetic) #will print 6.8
